c523322e1516ae59ade6233212d506f431d52fc3
RogersCodes <adindajohnrogers@gmail.com>
2024-11-12 22:16:32 +0300

    feat: added contact section to Blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Contact from "../components/contact";
 
 function Blog () {
     return (
         <div className="blog-page">
             <Navbar />
+            <Contact />
         </div>
     )
 }
