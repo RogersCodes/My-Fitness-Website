a62486e59ed51af5926d4dc0f3e86053bc7f467b
RogersCodes <adindajohnrogers@gmail.com>
2024-11-15 11:45:20 +0300

    fix: updated the label for signup page

--- src/components/SignUpComp/Signuppart.jsx
+++ src/components/SignUpComp/Signuppart.jsx
@@ -6,10 +6,12 @@ function SignUp () {
         <div className="signup">
             <h1 className="train-with-me">Train With Me, Rogers</h1>
             <h3 className="create-account">Create An Account</h3>
-            <form>
-                <label htmlFor="your-name">Your Name</label>
-                <input type="text" placeholder="First Name" className="first-name" required></input>
-                <input type="text" placeholder="Last Name" className="last-name" required></input>
+            <form className="signup-form">
+                <div className="name-details">
+                    <label htmlFor="your-name">Your Name</label>
+                    <input type="text" id="first-name" placeholder="First Name" className="first-name" required></input>
+                    <input type="text" id="last-name" placeholder="Last Name" className="last-name" required></input>
+                </div>
                 <label htmlFor="your-email">Your Email</label>
                 <input type="email" placeholder="email@address.com" className="your-email" required></input>
                 <input type="email" placeholder="Confirm Email" className="confirm-email" required></input>
@@ -17,7 +19,7 @@ function SignUp () {
                 <input type="password" id="password" className="password" placeholder="Enter Your Password"></input>
                 <button type="submit" className="new-account" aria-label="Continue to create a new account">Continue</button>
                 <p className="signup-policy">By continuing, you are indicating that you accept our <span className="terns-of-service">Terms of Service</span>and <span className="privacy-policy">Privacy Policy</span>, and consent to receiving emails from rogersfitnessclub.com.</p>
-                <Link to="/login"><button type="submit" className="login-button">Already a Member? Log In</button></Link>
+                <Link to="/login"><button type="button" className="login-button">Already a Member? Log In</button></Link>
             </form>
         </div>
     )
