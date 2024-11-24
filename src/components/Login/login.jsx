89eb077392a11396c6579cf384d4238f5aed0a30
RogersCodes <adindajohnrogers@gmail.com>
2024-11-24 18:38:07 +0300

    feat: added the header for the login page

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Login () {
     return (
-        <div></div>
+        <div className="login-page">
+            <h2 className="login-header">Log In</h2>
+            <p className="login-intro">Please login to continue</p>
+        </div>
     )
 }
 export default Login;
