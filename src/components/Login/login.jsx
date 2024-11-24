215f1830643533d0f1b6fc4af37e37189a1ba30f
RogersCodes <adindajohnrogers@gmail.com>
2024-11-24 19:02:18 +0300

    feat: added the email input for login page

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -5,6 +5,11 @@ function Login () {
         <div className="login-page">
             <h2 className="login-header">Log In</h2>
             <p className="login-intro">Please login to continue</p>
+            <form className="login-form">
+                <label htmlFor="login-email"></label>
+                <input type="text" id="login-email" placeholder="Your Email" className="login-email-address" name="loginEmail" required></input>
+                
+            </form>
         </div>
     )
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Login from "../components/Login/login";
 
 function LoginPage () {
     return (
         <div>
             <Navbar />
+            <Login />
         </div>
     )
 }
