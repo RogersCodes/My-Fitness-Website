5ee5a97106384b047dc81818f1ed6bbedde312f2
RogersCodes <adindajohnrogers@gmail.com>
2024-11-24 19:44:12 +0300

    feat: added the login and signup buttons to login page

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -8,7 +8,15 @@ function Login () {
             <form className="login-form">
                 <label htmlFor="login-email"></label>
                 <input type="text" id="login-email" placeholder="Your Email" className="login-email-address" name="loginEmail" required></input>
-                
+
+                <label htmlFor="login-password"></label>
+                <input type="password" id="login-password" className="login-page-password" aria-label="loginPassword" placeholder="**********" name="LoginPassword" required></input>
+
+                <button type="button" className="login-profile-button" aria-label="Proceed to your profile">Login</button>
+
+                <button type="button" className="reset-password">Reset Your Password</button>
+
+                <button type="button" className="join-now-button">Not A Member? Join Now!</button>
             </form>
         </div>
     )
