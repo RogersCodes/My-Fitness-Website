3ac134b75f037e5f1482579770eb1469b139e2c2
RogersCodes <adindajohnrogers@gmail.com>
2024-11-29 00:03:25 +0300

    feat: added the feature to change the user login inputs

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -8,6 +8,10 @@ function Login () {
 
     });
     const navigateLogin = useNavigate();
+    const handleLoginChange = (e) => {
+        const { name, value } = e.target;
+        setInputData((prevData) => ({...prevData, [name]: value}));
+    };
     const handleSignUpRedirect = () => {
         navigateLogin("/signup");
     }
@@ -17,10 +21,10 @@ function Login () {
             <p className="login-intro">Please login to continue</p>
             <form className="login-form">
                 <label htmlFor="login-email"></label>
-                <input type="text" id="login-email" placeholder="Your Email" className="login-email-address" name="loginEmail" required></input>
+                <input type="text" id="login-email" placeholder="Your Email" className="login-email-address" name="loginEmail" value={inputData.loginEmail} onChange={handleLoginChange} required></input>
 
                 <label htmlFor="login-password"></label>
-                <input type="password" id="login-password" className="login-page-password" aria-label="loginPassword" placeholder="**********" name="LoginPassword" required></input>
+                <input type="password" id="login-password" className="login-page-password" aria-label="loginPassword" placeholder="**********" name="LoginPassword" value={inputData.loginPassword} onChange={handleLoginChange} required></input>
 
                 <button type="button" className="login-profile-button" aria-label="Proceed to your profile">Login</button>
 
