186238f0caca706bbca0fa0e5802fbcdf832a93b
RogersCodes <adindajohnrogers@gmail.com>
2024-12-07 16:20:23 +0300

    feat: blogcard function

---
+++ src/components/Blogs/blogpage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+ 
+//Blogcard
+const BlogCard = () => {
+    return (
+        <div className="blog-card"></div>
+    )
+}
+export default BlogCard;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Contact from "../components/contact";
+import BlogCard from "../components/Blogs/blogpage";
 
 function Blog () {
     return (
         <div className="blog-page">
             <Navbar />
+            <BlogCard />
             <Contact />
         </div>
     )
