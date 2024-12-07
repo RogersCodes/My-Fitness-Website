64d2e1ef2b27d5bfbd270dae7367252125a9b180
RogersCodes <adindajohnrogers@gmail.com>
2024-12-07 17:00:45 +0300

    feat: added props to blogcard fn

--- src/components/Blogs/blogpage.jsx
+++ src/components/Blogs/blogpage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
  
 //Blogcard
-const BlogCard = () => {
+const BlogCard = ({ title, content, category, image }) => {
     return (
-        <div className="blog-card"></div>
-    )
-}
+        <div className="blog-card">
+            <img src={image} alt={title} className="blog-image"/>
+            <div className="blog-content">
+                <h3 className="blog-header">{title}</h3>
+                <p className="blog-paragraph">{content}</p>
+                <span className="category">{category}</span>
+            </div>
+        </div>
+    );
+};
 export default BlogCard;
