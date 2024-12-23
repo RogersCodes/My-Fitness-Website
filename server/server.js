5aad8c276f95c5b3645fee32bd69513d0b9b0d1b
RogersCodes <adindajohnrogers@gmail.com>
2024-12-23 18:27:13 +0300

    feat: Add server-side logic

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
-const app = express
+const app = express();
 
-//
+//Middleware functions
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Connnect to MongoDb
+connectDB();
+
+//Routes
+app.get('/', (req, res) => res.send('Fitness API is running'));
+
+//Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
