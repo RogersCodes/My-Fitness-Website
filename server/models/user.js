4595f4dee45a020cae80d0348fa0ceda14cf997e
RogersCodes <adindajohnrogers@gmail.com>
2024-12-23 20:51:07 +0300

    feat: Add userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -1,2 +1,43 @@
 const mongoose = require('mongoose');
-const b
+const bcrypt = require('bcryptjs');
+const { Schema } = mongoose;
+
+//The user schema
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unqiue: true,
+        lowercase: true,
+        validate: {
+            validator: (value) => {
+                //validate email regex fn
+                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+            },
+            message: 'Plase enter a valid email address',
+        },
+    },
+    confirmEmail: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value === this.email;
+            },
+            message: 'Email addresses do not match',
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+});
+
+//
