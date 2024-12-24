6e774b00b8d8eb050ecb5e38af758ffb2b1f1537
RogersCodes <adindajohnrogers@gmail.com>
2024-12-24 15:32:05 +0300

    feat: Add login auth to server code

--- server/server.js
+++ server/server.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) => res.send('Fitness API is running'));
 app.use('/api/auth', authRoutes);
 
 //Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
