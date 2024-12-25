0dc5bc19974b93745624f9e20f7249874da4c5d3
RogersCodes <adindajohnrogers@gmail.com>
2024-12-25 22:52:49 +0300

    feat: Axios for pulling API from backend

--- client/src/components/SignUpComp/Signuppart.jsx
+++ client/src/components/SignUpComp/Signuppart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 function SignUp () {
     const [formData, setFormData] = useState({
         firstName: "",
