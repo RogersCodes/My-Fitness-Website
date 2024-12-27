86376ccceb4a0ba4a89ed5b68d75b8b1af5a635c
RogersCodes <adindajohnrogers@gmail.com>
2024-12-27 14:59:21 +0300

    fix: Addresed an error in the name attribute of login password

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -39,7 +39,7 @@ function Login () {
                 <input type="text" id="login-email" placeholder="Your Email" className="login-email-address" name="loginEmail" value={inputData.loginEmail} onChange={handleLoginChange} required></input>
 
                 <label htmlFor="login-password"></label>
-                <input type="password" id="login-password" className="login-page-password" aria-label="loginPassword" placeholder="**********" name="LoginPassword" value={inputData.loginPassword} onChange={handleLoginChange} required></input>
+                <input type="password" id="login-password" className="login-page-password" aria-label="loginPassword" placeholder="**********" name="loginPassword" value={inputData.loginPassword} onChange={handleLoginChange} required></input>
 
                 <button type="button" className="login-profile-button" aria-label="Proceed to your profile">Login</button>
 
