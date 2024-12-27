dc2dd754c64de27c22416474b5f64ff7ca47998f
RogersCodes <adindajohnrogers@gmail.com>
2024-12-27 16:36:55 +0300

    feat: Created a dashboard page

---
+++ client/src/pages/DashBoard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Dashboard from "../components/Dashboard/Dashboard";
+
+function Dashboardw () {
+    return (
+        <div>
+            <Dashboard />
+        </div>
+    )
+}
+export default Dashboardw;
