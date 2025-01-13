01494a395ce280de58ddcdbaf9e2ee6a0e24af89
RogersCodes <adindajohnrogers@gmail.com>
2025-01-13 14:20:28 +0300

    feat: Edited the proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,14 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true, 
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    open: true,
   },
   build: {
     outDir: 'build',
