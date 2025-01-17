d39cc8d2048d4c4a9a45a6ba3aab4e90a94708df
RogersCodes <adindajohnrogers@gmail.com>
2025-01-17 23:17:17 +0300

    feat: Added comment

--- client/src/components/SignUpComp/Signuppart.jsx
+++ client/src/components/SignUpComp/Signuppart.jsx
@@ -16,7 +16,7 @@ function SignUp () {
     const signupUser = async (data) => {
         try {
             //Debug feature
-            console.log('Attempting to send data:', data);
+            //console.log('Attempting to send data:', data);
             //error is here
             const response = await fetch("http://localhost:8080/api/auth/signup", {
                 method: "POST",
