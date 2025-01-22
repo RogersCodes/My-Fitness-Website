35484eaa8af46ba513c72650d5a3ae7794defb07
RogersCodes <adindajohnrogers@gmail.com>
2025-01-22 23:35:06 +0300

    feat: Added footer to dashboard

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Dashboard from "../components/Dashboard/Dashboard";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 function Dashboardw () {
     return (
         <div>
+            <Navbar />
             <Dashboard />
+            <Footer />
         </div>
     )
 }
