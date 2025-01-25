e76637d673cabca0bc99384a2c2549d09833bab8
RogersCodes <adindajohnrogers@gmail.com>
2025-01-25 13:40:34 +0300

    fix: Error in login page

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -11,7 +11,7 @@ function Login () {
     const navigateLogin = useNavigate();
     const loginUser = async (email, password) => {
         try {
-            const response = await fetch("http://localhost:8080/api/login", {
+            const response = await fetch("http://localhost:8080/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
