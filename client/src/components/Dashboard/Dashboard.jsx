ec48561c3a0bb83ebf9e88d7a1a2cbc7a0b0e74b
RogersCodes <adindajohnrogers@gmail.com>
2025-03-01 20:33:59 +0300

    fix: Error in dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -21,6 +21,7 @@ const Dashboard = () => {
     //logout option
     const handleLogout = () => {
         localStorage.removeItem("token");
+        localStorage.removeItem("user");
         window.location.href = '/';
     };
 //add videos and images to the dashboard
