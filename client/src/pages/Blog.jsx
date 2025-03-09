d59b69bb4f6c121b562c95dc9306a2a346d9ef0e
RogersCodes <adindajohnrogers@gmail.com>
2025-03-09 12:43:09 +0300

    fix: Removed uneccessary components in blog page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Navbar from "../components/HomePage/navbar";
 import Contact from "../components/HomePage/contact";
-import BlogCard from "../components/Blogs/BlogCard";
 import BlogSection from "../components/Blogs/BlogSection";
-import CategoryBlogs from "../components/Blogs/CategoryBlogs";
-import BlogDetails from "../components/Blogs/BlogDetails";
+
 
 
 function Blog() {
