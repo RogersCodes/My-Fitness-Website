36c582736933d1ca96fb3f7ab020a0d148cec6e0
RogersCodes <adindajohnrogers@gmail.com>
2025-03-16 20:23:19 +0300

    fix: Styling homepage

--- client/src/components/HomePage/navbar.jsx
+++ client/src/components/HomePage/navbar.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, useNavigate } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi";
+
 
 function Navbar() {
   const navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(false);
 
   return (
     <header>
       <nav className="navbar">
         <div className="navbar-container">
           <h2 className="header-title">Rogers Fitness Club</h2>
+          {/*Hamburger menu icon*/}
+          <div className="hamburger-menu" onClick={() => setIsMobile(!isMobile)}>
+            {isMobile ? <FiX size={30} color="#fff" /> : <FiMenu size={30} color="#fff"/>}
+          </div>
           <ul className="navbar-list">
             <li><NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink></li>
             <li><NavLink to="/workouts" className={({ isActive }) => (isActive ? "active" : "")}>Workouts</NavLink></li>
