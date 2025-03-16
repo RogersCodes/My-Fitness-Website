d1896ca575549c7d351681d3219728c7a9430c01
RogersCodes <adindajohnrogers@gmail.com>
2025-03-17 00:43:39 +0300

    fix: Fixed responsiveness issue in contact

--- client/src/components/HomePage/contact.jsx
+++ client/src/components/HomePage/contact.jsx
@@ -10,8 +10,8 @@ function Contact() {
                 <li className="contact-list-item"><a href="/nutrition">Nutrition</a></li>
             </ul>
             <div className="contact-images">
-                <a href="https://www.instagram.com/dinx_roger/" target="_blank" rel="noopener noreferrer"><img src="images/Group 2.png" alt="instagram-icon" className="instagram-image"/></a>
-                <a href="https://x.com/RawG_Dinx" target="_blank" rel="noopener noreferrer"><img src="images/Vector.png" alt="x-icon" className="x-image"/></a>
+                <a href="https://www.instagram.com/dinx_roger/" target="_blank" rel="noopener noreferrer" aria-label="Visit my Instagram profile"><img src="images/Group 2.png" alt="instagram-icon" className="instagram-image"/></a>
+                <a href="https://x.com/RawG_Dinx" target="_blank" rel="noopener noreferrer" aria-label="Visit my Twitter (X) profile"><img src="images/Vector.png" alt="x-icon" className="x-image"/></a>
                 <a href="mailto:adindajohnrogers@gmail.com"><img src="images/Group.png" alt="email-icon" className="email-image" /></a>
             </div>
             
